75381dbed160b921844e82f1dc007fa54eb823a6
vishnumohanrk <mohanvish1999@gmail.com>
2021-06-01 08:16:16 +0530

    ..

--- topics/factorial/iterative.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-  if n < 0:
-    return None
-
-  prod = 1
-  for i in range(1, n + 1):
-    prod = prod * i
-
-  return prod
--- topics/factorial/recursive.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-  if n < 0:
-    return None
-
-  if n <= 1:
-    return 1
-
-  return n * factorial(n - 1)
